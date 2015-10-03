cbfc8593f41029aa5bd1b87e944200f982cd7234
Joao Antonio Ferreira <joao.parana@gmail.com>
2015-10-03 11:57:43 -0300

    Primeiro commit

---
+++ list-directories.sh
@@ -0,0 +1,7 @@
+echo "••• `date` - Diretório Corrente: `pwd` " && \
+echo "••• `date` - listando conteudo de /var/www/html " && \
+ls -la /var/www/html && \
+echo "••• `date` - listando conteudo de /var/www/html/moodle " && \
+ls -la /var/www/html/moodle && \
+echo "••• `date` - listando conteudo de /var/moodle-data " && \
+ls -la /var/moodle-data
